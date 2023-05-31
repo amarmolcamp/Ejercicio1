54581b08774091ac45cafe171876192cce9bb2f4
amarmolcamp <alberto.marmol@campusfp.es>
2023-05-31 23:39:06 +0200

    Primer comitt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/inicio' , pathMatch: 'full'},
+  {path: 'inicio', component:InicioComponent},  
+  {path: 'contacto', component:ContactoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    MenuComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
